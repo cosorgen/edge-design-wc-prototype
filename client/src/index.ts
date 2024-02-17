a5f632047b620b0259ce710a1bab837e32a93003
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2024-02-17 15:13:38 -0800

    Working dev serve

---
+++ client/src/index.ts
@@ -0,0 +1,6 @@
+import { setTheme } from '@fluentui/web-components';
+import { phoenixLightThemeWin11 } from '@phoenixui/themes';
+
+setTheme(phoenixLightThemeWin11);
+console.log('tester the bobby');
+``
