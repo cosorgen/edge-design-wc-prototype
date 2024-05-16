58cebc50dce4336b9efc065c83e7ebbb6dcf07c6
Tushar Gupta <tush1998@gmail.com>
2024-05-16 11:46:35 -0700

    adding caption controls on the right
    changes made:
    - review new div created called "left-aligned-container"
    - new icons added for caption controls

--- client/src/edge/views/tabBar.ts
+++ client/src/edge/views/tabBar.ts
@@ -25,41 +25,72 @@ const template = html<TabBar>`
   <mica-material appearance="tabBar"></mica-material>
   <div id="shadow"></div>
   <div id="content">
-    <div class="group">
-      <identity-control></identity-control>
-    </div>
-    <div class="group">
-      <phx-button appearance="subtle" icon-only>
+    <div id="left-aligned-container">
+      <div class="group">
+        <identity-control></identity-control>
+      </div>
+      <div class="group">
+        <phx-button appearance="subtle" icon-only>
+          <svg>
+            <use href="img/edge/icons.svg#layer-diagonal-20-regular"></use>
+          </svg>
+        </phx-button>
+        <phx-button appearance="subtle" icon-only>
+          <svg>
+            <use
+              href="img/edge/icons.svg#tab-position-horizontal-20-regular"
+            ></use>
+          </svg>
+        </phx-button>
+      </div>
+      <div id="tabs">
+        ${repeat(
+          (x) => x.ts.tabs,
+          html<Tab>` <horizontal-tab
+            ?active="${(x) => x.active}"
+            @activate="${(x, c) => c.parent.activateTab(x.id)}"
+            @close="${(x, c) => c.parent.closeTab(x.id)}"
+          >
+            ${(x) => x.title}
+            <img slot="favicon" src="${(x) => x.favicon}" />
+          </horizontal-tab>`,
+        )}
+      </div>
+      <phx-button appearance="subtle" icon-only id="add">
         <svg>
-          <use href="img/edge/icons.svg#layer-diagonal-20-regular"></use>
+          <use href="img/edge/icons.svg#add-20-regular"></use>
         </svg>
       </phx-button>
-      <phx-button appearance="subtle" icon-only>
-        <svg>
-          <use
-            href="img/edge/icons.svg#tab-position-horizontal-20-regular"
-          ></use>
-        </svg>
-      </phx-button>
     </div>
-    <div id="tabs">
-      ${repeat(
-        (x) => x.ts.tabs,
-        html<Tab>` <horizontal-tab
-          ?active="${(x) => x.active}"
-          @activate="${(x, c) => c.parent.activateTab(x.id)}"
-          @close="${(x, c) => c.parent.closeTab(x.id)}"
-        >
-          ${(x) => x.title}
-          <img slot="favicon" src="${(x) => x.favicon}" />
-        </horizontal-tab>`,
-      )}
+    <div class="group">
+    <phx-button
+    size="large"
+    appearance="subtle"
+    icon-only
+  >
+    <svg>
+      <use href="img/edge/icons.svg#minimize-16-regular" x="4" y="4"></use>
+    </svg>
+  </phx-button>
+    <phx-button
+    size="large"
+    appearance="subtle"
+    icon-only
+  >
+    <svg>
+      <use href="img/edge/icons.svg#maximize-16-regular"  x="4" y="4"></use>
+    </svg>
+  </phx-button>
+    <phx-button
+    size="large"
+    appearance="subtle"
+    icon-only
+  >
+    <svg>
+      <use href="img/edge/icons.svg#dismiss-16-regular" x="4" y="4"></use>
+    </svg>
+  </phx-button>
     </div>
-    <phx-button appearance="subtle" icon-only id="add">
-      <svg>
-        <use href="img/edge/icons.svg#add-20-regular"></use>
-      </svg>
-    </phx-button>
   </div>
 `;
 
@@ -70,16 +101,23 @@ const styles = css`
     overflow: hidden;
   }
 
-  #content {
+  #left-aligned-container {
     position: relative;
     box-sizing: border-box;
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
     align-items: flex-end;
     gap: ${spacingHorizontalS};
     padding-inline: ${spacingHorizontalXS};
     padding-block-start: ${spacingVerticalSNudge};
     padding-block-end: ${spacingVerticalXXS};
+  }
+
+  #content {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
   }
 
   .group {
