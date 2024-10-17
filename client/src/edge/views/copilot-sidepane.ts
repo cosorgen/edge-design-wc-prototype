dae592fdfd0737ea0895f49e81a237d5c6d4a506
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2024-10-17 12:36:59 -0700

    Copilot dark theme

--- client/src/edge/views/copilot-sidepane.ts
+++ client/src/edge/views/copilot-sidepane.ts
@@ -1,9 +1,40 @@
 import EdgeWindowService from '#servicesedgeWindowService.js';
-import { html, css, FASTElement, customElement } from '@microsoft/fast-element';
+import {
+  html,
+  css,
+  FASTElement,
+  customElement,
+  Observable,
+} from '@microsoft/fast-element';
 import { inject } from '@microsoft/fast-element/di.js';
-import { spacingHorizontalXXL, spacingVerticalXXL } from '@phoenixui/themes';
+import {
+  colorNeutralCardBackground,
+  colorNeutralForeground1,
+  phoenixDarkThemeWin11,
+  phoenixLightThemeWin11,
+  spacingHorizontalXXL,
+  spacingVerticalXXL,
+} from '@phoenixui/themes';
 import '../controls/sidepane-header.js';
 import '../controls/copilot-composer.js';
+import { setThemeFor } from '@phoenixui/web-components';
+import EdgeSettingsSerivce from '#servicessettingsService.js';
+import WindowsService from '#serviceswindowsService.js';
+
+const copilotLightTheme = {
+  ...phoenixLightThemeWin11,
+  backgroundGradient:
+    '180deg, #FCF9F6 0%, #FCF9F6 60%, #FBEBE0 99%, #FDE5CD 100%',
+  colorNeutralForeground1: '#33302E',
+  colorNeutralCardBackground: '#fee5ce',
+};
+const copilotDarkTheme = {
+  ...phoenixDarkThemeWin11,
+  backgroundGradient:
+    '180deg, rgba(16, 21, 36, 0.8) 0%, rgba(16, 21, 36, 0.8) 80%, rgba(16, 21, 36, 0.8) 100%',
+  colorNeutralForeground1: '#F2DDCC',
+  colorNeutralCardBackground: '#333333',
+};
 
 const template = html`
   <sidepane-header>Copilot</sidepane-header>
@@ -49,13 +80,8 @@ const styles = css`
     flex-direction: column;
     height: 100%;
     width: 100%;
-    background: linear-gradient(
-      180deg,
-      #fcf9f6 0%,
-      #fcf9f6 60%,
-      #fbebe0 99%,
-      #fde5cd 100%
-    );
+    background: linear-gradient(var(--backgroundGradient));
+    color: ${colorNeutralForeground1};
   }
 
   #content {
@@ -81,7 +107,7 @@ const styles = css`
   }
 
   .user {
-    background-color: #fee5ce;
+    background-color: ${colorNeutralCardBackground};
     padding: 14px 20px;
     width: fit-content;
     border-radius: 12px;
@@ -96,13 +122,43 @@ const styles = css`
 })
 export class CopilotSidepane extends FASTElement {
   @inject(EdgeWindowService) ews!: EdgeWindowService;
+  @inject(EdgeSettingsSerivce) ess!: EdgeSettingsSerivce;
+  @inject(WindowsService) ws!: WindowsService;
 
   connectedCallback() {
     super.connectedCallback();
     this.addEventListeners();
+    this.setTheme();
   }
 
   addEventListeners() {
-    this.addEventListener('close', () => this.ews.closeSidepaneApp());
+    this.addEventListener('close', this.handleClose);
+    Observable.getNotifier(this.ess).subscribe(this, 'theme');
+    Observable.getNotifier(this.ws).subscribe(this, 'theme');
+  }
+
+  removeEventListeners() {
+    this.removeEventListener('close', this.handleClose);
+    Observable.getNotifier(this.ess).unsubscribe(this);
+    Observable.getNotifier(this.ws).unsubscribe(this);
+  }
+
+  handleClose = () => {
+    this.ews.closeSidepaneApp();
+  };
+
+  handleChange(target: unknown, property: string) {
+    if (property === 'theme') this.setTheme();
+  }
+
+  setTheme() {
+    const themes = {
+      light: copilotLightTheme,
+      dark: copilotDarkTheme,
+    };
+    const themeSelection =
+      this.ess.theme === 'system' ? this.ws.theme : this.ess.theme;
+
+    setThemeFor(this.shadowRoot!, themes[themeSelection]);
   }
 }
