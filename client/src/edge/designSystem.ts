c93df0eb5084b89810ac0d3460195ee1a18b85f7
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2024-11-12 16:56:44 -0800

    Removing design system overrides

--- client/src/edge/designSystem.ts
+++ client/src/edge/designSystem.ts
@@ -7,53 +7,33 @@ import {
 } from '@phoenixui/themes';
 
 export type NewTokens = {
-  colorLayerBackgroundPillMenu: string;
   spacingFrame: string;
-  colorLayerOmniboxBackground: string;
-  colorLayerOmniboxBackgroundHover: string;
 };
 
 export type EdgeTheme = Theme & NewTokens;
 
 const commonThemeOverrides = {
-  borderRadiusLayerApp: '12px',
   spacingFrame: '4px',
 };
 
 export const edgeLightTheme: EdgeTheme = {
   ...phoenixLightThemeWin11,
   ...commonThemeOverrides,
-  colorLayerBackgroundPillMenu: '#ffffff80',
-  colorLayerBackgroundApp: '#dde2e8',
-  colorLayerOmniboxBackground: '#F1F3F6',
-  colorLayerOmniboxBackgroundHover: '#EBEEF1',
 };
 
 export const edgeLightThemeSolid: EdgeTheme = {
   ...phoenixLightThemeSolidWin11,
   ...commonThemeOverrides,
-  colorLayerBackgroundPillMenu: '#ffffff80',
-  colorLayerBackgroundApp: '#dde2e8',
-  colorLayerOmniboxBackground: '#F1F3F6',
-  colorLayerOmniboxBackgroundHover: '#EBEEF1',
 };
 
 export const edgeDarkTheme: EdgeTheme = {
   ...phoenixDarkThemeWin11,
   ...commonThemeOverrides,
-  colorLayerBackgroundPillMenu: '#2C2C2C',
-  colorLayerBackgroundApp: '#000',
-  colorLayerOmniboxBackground: '#0E0E0E',
-  colorLayerOmniboxBackgroundHover: '#000000',
 };
 
 export const edgeDarkThemeSolid: EdgeTheme = {
   ...phoenixDarkThemeSolidWin11,
   ...commonThemeOverrides,
-  colorLayerBackgroundPillMenu: '#2C2C2C',
-  colorLayerBackgroundApp: '#000',
-  colorLayerOmniboxBackground: '#0E0E0E',
-  colorLayerOmniboxBackgroundHover: '#000000',
 };
 
 export const colorLayerBackgroundPillMenu =
