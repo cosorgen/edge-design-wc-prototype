0dc82af31c864fe3f88ab84802e0185640355889
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2025-03-23 13:54:11 -0700

    Fixed f2 token formatting

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -1,14 +1,8 @@
-function camelCaseToKebabCase(str: string) {
-  return str
-    .replace(/[A-Z]/gm, (letter) => `-${letter.toLowerCase()}`)
-    .replace(/([0-9])[0-9]*/gm, (number) => `-${number}`);
-}
-
 export function themeToTokensObject<T>(theme: T) {
   const tokens: Record<string, string> = {};
   const keys = Object.keys(theme as Record<string, unknown>);
   for (const key of keys) {
-    tokens[key] = `var(--${camelCaseToKebabCase(key)})`;
+    tokens[key] = `var(--${key})`;
   }
   return tokens;
 }
@@ -21,7 +15,7 @@ export function setThemeFor<Theme>(
   const tokens = Object.keys(theme as Record<string, unknown>);
   let data = '';
   tokens.forEach((name) => {
-    data += `--${camelCaseToKebabCase(name)}: ${String(theme[name as keyof Theme])};`;
+    data += `--${name}: ${String(theme[name as keyof Theme])};`;
   });
   sheet.replaceSync(
     `:${element instanceof ShadowRoot ? 'host' : 'root'} {${data}}`,
