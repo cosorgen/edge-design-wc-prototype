76c25395ff3f0d6df33ffecfd57a4d98d1484b4e
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2025-03-23 16:22:11 -0700

    Splitting out types and const, prob wrong structure

--- packages/kumo-theme/src/globalColor.ts
+++ packages/kumo-theme/src/globalColor.ts
@@ -1,106 +1,11 @@
-export type Neutral = {
-  0: string; // white
-  4: string;
-  8: string;
-  100: string;
-  104: string; // hover is +4
-  108: string; // pressed is +8
-  150: string;
-  154: string;
-  158: string;
-  200: string;
-  250: string;
-  300: string;
-  304: string;
-  308: string;
-  350: string;
-  400: string;
-  404: string;
-  408: string;
-  450: string;
-  454: string;
-  458: string;
-  500: string;
-  550: string;
-  554: string;
-  558: string;
-  600: string;
-  650: string;
-  700: string;
-  704: string;
-  708: string;
-  750: string;
-  754: string;
-  758: string;
-  800: string;
-  804: string;
-  808: string;
-  1000: string; // black
-};
-
-export type Vibrant = {
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-  900: string;
-};
-
-export type AlphaBlack = {
-  5: string;
-  10: string;
-  20: string;
-  30: string;
-  40: string;
-  50: string;
-  60: string;
-  70: string;
-  80: string;
-  90: string;
-};
-
-export type AlphaWhite = {
-  5: string;
-  10: string;
-  20: string;
-  30: string;
-  40: string;
-  50: string;
-  60: string;
-  70: string;
-  80: string;
-  90: string;
-};
-
-export type AlphaVibrant100 = {
-  5: string;
-  10: string;
-  20: string;
-  30: string;
-  40: string;
-  50: string;
-  60: string;
-  70: string;
-  80: string;
-  90: string;
-};
-
-export type AlphaVibrant800 = {
-  5: string;
-  10: string;
-  20: string;
-  30: string;
-  40: string;
-  50: string;
-  60: string;
-  70: string;
-  80: string;
-  90: string;
-};
+import type {
+  Neutral,
+  Vibrant,
+  AlphaBlack,
+  AlphaWhite,
+  AlphaVibrant100,
+  AlphaVibrant800,
+} from './types/globalColor.js';
 
 export const neutral: Neutral = {
   0: '#ffffff',
---
+++ packages/kumo-theme/src/index.ts
@@ -0,0 +1,17 @@
+export type {
+  Neutral,
+  Vibrant,
+  AlphaBlack,
+  AlphaWhite,
+  AlphaVibrant100,
+  AlphaVibrant800,
+} from './types/globalColor.js';
+
+export {
+  neutral,
+  vibrant,
+  alphaBlack,
+  alphaWhite,
+  alphaVibrant100,
+  alphaVibrant800,
+} from './globalColor.js';
