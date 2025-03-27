3cdafcad041c39b75fe88c82282285f8e7246d87
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2025-03-26 18:11:01 -0700

    Fix padding

--- packages/kumo-theme/src/padding.ts
+++ packages/kumo-theme/src/padding.ts
@@ -1,27 +1,33 @@
 import { size } from './globalValues.js';
 
 export type PaddingCtrl = {
+  paddingControlDefaultHorizontalIconOnly: string;
+  paddingControlDefaultToNestedControl: string;
+  paddingControlHorizontalDefault: string;
+  paddingControlTextTop: string;
   paddingCtrlHorizontalDefault: string;
-  paddingCtrlHorizontalIconOnly: string;
-  paddingCtrlTextTop: string;
-  paddingCtrlTextBottom: string;
-  paddingCtrlTextSide: string;
-  paddingCtrlToNestedControl: string;
+  paddingCtrlHorizontalIcononly: string;
+  paddingCtrlTextbottom: string;
+  paddingCtrlTextside: string;
+  paddingCtrlTexttop: string;
+  paddingCtrlTonestedcontrol: string;
+  paddingTextHorizontal: string;
+  paddingToNestedControl: string;
 };
 
 export type PaddingCtrlSm = {
   paddingCtrlSmHorizontalDefault: string;
-  paddingCtrlSmHorizontalIconOnly: string;
-  paddingCtrlSmTextTop: string;
+  paddingCtrlSmHorizontalIcononly: string;
   paddingCtrlSmTextBottom: string;
+  paddingCtrlSmTextTop: string;
   paddingCtrlSmToNestedControl: string;
 };
 
 export type PaddingCtrlLg = {
   paddingCtrlLgHorizontalDefault: string;
-  paddingCtrlLgHorizontalIconOnly: string;
-  paddingCtrlLgTexttop: string;
+  paddingCtrlLgHorizontalIcononly: string;
   paddingCtrlLgTextbottom: string;
+  paddingCtrlLgTexttop: string;
   paddingCtrlLgToNestedControl: string;
 };
 
@@ -31,53 +37,60 @@ export type PaddingFlyout = {
 
 export type PaddingCard = {
   paddingCard: string;
-  paddingCardNestedImage: string;
+  paddingCardNestedimage: string;
 };
 
 export type PaddingContent = {
-  paddingContentNone: string;
-  paddingContentXxxSmall: string;
-  paddingContentXxsmall: string;
-  paddingContentXSmallNudge: string;
-  paddingContentXSmall: string;
-  paddingContentSmall: string;
-  paddingContentMedium: string;
-  paddingContentLarge: string;
-  paddingContentXLarge: string;
-  paddingContentXxLarge: string;
-  paddingContentXxxLarge: string;
   paddingContentAlignDefault: string;
-  paddingContentAlignOutdentIconOnSubtle: string;
-  paddingContentAlignOutdentTextOnSubtle: string;
+  paddingContentAlignOutdentIcononsubtle: string;
+  paddingContentAlignOutdentTextonsubtle: string;
+  paddingContentLarge: string;
+  paddingContentMedium: string;
+  paddingContentNone: string;
+  paddingContentSmall: string;
+  paddingContentXLarge: string;
+  paddingContentXSmall: string;
+  paddingContentXSmallNudge: string;
+  paddingContentXxLarge: string;
+  paddingContentXxsmall: string;
+  paddingContentXxxLarge: string;
+  paddingContentXxxSmall: string;
 };
 
 export type PaddingToolbar = {
+  paddingToolbar: string;
   paddingToolbarInside: string;
   paddingToolbarOutside: string;
 };
 
 export const paddingCtrl: PaddingCtrl = {
+  paddingControlDefaultHorizontalIconOnly: '{paddingCtrlHorizontalIcononly}',
+  paddingControlDefaultToNestedControl: '{paddingCtrlToNestedControl}',
+  paddingControlHorizontalDefault: '{paddingCtrlHorizontalDefault}',
+  paddingControlTextTop: '{paddingCtrlTexttop}',
   paddingCtrlHorizontalDefault: size[80],
-  paddingCtrlHorizontalIconOnly: size[60],
-  paddingCtrlTextTop: size[60],
-  paddingCtrlTextBottom: '{paddingCtrlTextTop}',
-  paddingCtrlTextSide: size[20],
-  paddingCtrlToNestedControl: size[40],
+  paddingCtrlHorizontalIcononly: size[60],
+  paddingCtrlTextbottom: '{paddingCtrlTextTop}',
+  paddingCtrlTextside: size[20],
+  paddingCtrlTexttop: size[60],
+  paddingCtrlTonestedcontrol: size[40],
+  paddingTextHorizontal: '{paddingCtrlHorizontalDefault}',
+  paddingToNestedControl: '{paddingCtrlToNestedControl}',
 };
 
 export const paddingCtrlSm: PaddingCtrlSm = {
   paddingCtrlSmHorizontalDefault: size[80],
-  paddingCtrlSmHorizontalIconOnly: size[20],
-  paddingCtrlSmTextTop: size[40],
+  paddingCtrlSmHorizontalIcononly: size[20],
   paddingCtrlSmTextBottom: '{paddingCtrlSmTextTop}',
+  paddingCtrlSmTextTop: size[40],
   paddingCtrlSmToNestedControl: size[0],
 };
 
 export const paddingCtrlLg: PaddingCtrlLg = {
   paddingCtrlLgHorizontalDefault: size[80],
-  paddingCtrlLgHorizontalIconOnly: size[80],
-  paddingCtrlLgTexttop: size[80],
+  paddingCtrlLgHorizontalIcononly: size[80],
   paddingCtrlLgTextbottom: '{paddingCtrlLgTexttop}',
+  paddingCtrlLgTexttop: size[80],
   paddingCtrlLgToNestedControl: size[40],
 };
 
@@ -87,27 +100,28 @@ export const paddingFlyout: PaddingFlyout = {
 
 export const paddingCard: PaddingCard = {
   paddingCard: size[200],
-  paddingCardNestedImage: '{paddingCard}',
+  paddingCardNestedimage: '{paddingCard}',
 };
 
 export const paddingContent: PaddingContent = {
-  paddingContentNone: size[0],
-  paddingContentXxxSmall: size[20],
-  paddingContentXxsmall: size[40],
-  paddingContentXSmallNudge: size[60],
-  paddingContentXSmall: size[80],
-  paddingContentSmall: size[120],
-  paddingContentMedium: size[160],
-  paddingContentLarge: size[200],
-  paddingContentXLarge: size[240],
-  paddingContentXxLarge: size[320],
-  paddingContentXxxLarge: size[400],
   paddingContentAlignDefault: size[200],
-  paddingContentAlignOutdentIconOnSubtle: size[120],
-  paddingContentAlignOutdentTextOnSubtle: size[100],
+  paddingContentAlignOutdentIcononsubtle: size[120],
+  paddingContentAlignOutdentTextonsubtle: size[100],
+  paddingContentLarge: size[200],
+  paddingContentMedium: size[160],
+  paddingContentNone: size[0],
+  paddingContentSmall: size[120],
+  paddingContentXLarge: size[240],
+  paddingContentXSmall: size[80],
+  paddingContentXSmallNudge: size[60],
+  paddingContentXxLarge: size[320],
+  paddingContentXxsmall: size[40],
+  paddingContentXxxLarge: size[400],
+  paddingContentXxxSmall: size[20],
 };
 
 export const paddingToolbar: PaddingToolbar = {
+  paddingToolbar: '{paddingToolbarInside}',
   paddingToolbarInside: size[80],
   paddingToolbarOutside: size[40],
 };
