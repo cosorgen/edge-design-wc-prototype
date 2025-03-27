d05c398e63c098f55197a1197cc891330c8bc85f
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2025-03-26 17:09:20 -0700

    shadow

--- packages/kumo-theme/src/shadow.ts
+++ packages/kumo-theme/src/shadow.ts
@@ -1,143 +1,145 @@
 import { shadow as s } from './globalValues.js';
 
 export type ShadowParts = {
-  shadowFlyoutKeyX: string;
-  shadowFlyoutKeyY: string;
-  shadowFlyoutKeyBlur: string;
-  shadowFlyoutAmbientX: string;
-  shadowFlyoutAmbientY: string;
-  shadowFlyoutAmbientBlur: string;
-  shadowToolbarKeyX: string;
-  shadowToolbarKeyY: string;
-  shadowToolbarKeyBlur: string;
-  shadowToolbarAmbientX: string;
-  shadowToolbarAmbientY: string;
-  shadowToolbarAmbientBlur: string;
-  shadowCardRestKeyX: string;
-  shadowCardRestKeyY: string;
-  shadowCardRestKeyBlur: string;
-  shadowCardRestAmbientX: string;
-  shadowCardRestAmbientY: string;
-  shadowCardRestAmbientBlur: string;
-  shadowCardHoverKeyX: string;
-  shadowCardHoverKeyY: string;
-  shadowCardHoverKeyBlur: string;
-  shadowCardPressedKeyX: string;
-  shadowCardPressedKeyY: string;
-  shadowCardPressedKeyBlur: string;
+  shadowCardDisabledKeyBlur: string;
   shadowCardDisabledKeyX: string;
   shadowCardDisabledKeyY: string;
-  shadowCardDisabledKeyBlur: string;
-  shadowCtrlOnDragKeyX: string;
-  shadowCtrlOnDragKeyY: string;
-  shadowCtrlOnDragKeyBlur: string;
+  shadowCardHoverKeyBlur: string;
+  shadowCardHoverKeyX: string;
+  shadowCardHoverKeyY: string;
+  shadowCardPressedKeyBlur: string;
+  shadowCardPressedKeyX: string;
+  shadowCardPressedKeyY: string;
+  shadowCardRestAmbientBlur: string;
+  shadowCardRestAmbientX: string;
+  shadowCardRestAmbientY: string;
+  shadowCardRestKeyBlur: string;
+  shadowCardRestKeyX: string;
+  shadowCardRestKeyY: string;
+  shadowCtrlOnDragAmbientBlur: string;
   shadowCtrlOnDragAmbientX: string;
   shadowCtrlOnDragAmbientY: string;
-  shadowCtrlOnDragAmbientBlur: string;
-  shadowLayerKeyX: string;
-  shadowLayerKeyY: string;
-  shadowLayerKeyBlur: string;
+  shadowCtrlOnDragKeyBlur: string;
+  shadowCtrlOnDragKeyX: string;
+  shadowCtrlOnDragKeyY: string;
+  shadowFlyoutAmbientBlur: string;
+  shadowFlyoutAmbientX: string;
+  shadowFlyoutAmbientY: string;
+  shadowFlyoutKeyBlur: string;
+  shadowFlyoutKeyX: string;
+  shadowFlyoutKeyY: string;
+  shadowLayerAmbientBlur: string;
   shadowLayerAmbientX: string;
   shadowLayerAmbientY: string;
-  shadowLayerAmbientBlur: string;
-  shadowWindowActiveKeyX: string;
-  shadowWindowActiveKeyY: string;
-  shadowWindowActiveKeyBlur: string;
+  shadowLayerKeyBlur: string;
+  shadowLayerKeyX: string;
+  shadowLayerKeyY: string;
+  shadowToolbarAmbientBlur: string;
+  shadowToolbarAmbientX: string;
+  shadowToolbarAmbientY: string;
+  shadowToolbarKeyBlur: string;
+  shadowToolbarKeyX: string;
+  shadowToolbarKeyY: string;
+  shadowWindowActiveAmbientBlur: string;
   shadowWindowActiveAmbientX: string;
   shadowWindowActiveAmbientY: string;
-  shadowWindowActiveAmbientBlur: string;
-  shadowWindowInactiveKeyX: string;
-  shadowWindowInactiveKeyY: string;
-  shadowWindowInactiveKeyBlur: string;
+  shadowWindowActiveKeyBlur: string;
+  shadowWindowActiveKeyX: string;
+  shadowWindowActiveKeyY: string;
+  shadowWindowInactiveAmbientBlur: string;
   shadowWindowInactiveAmbientX: string;
   shadowWindowInactiveAmbientY: string;
-  shadowWindowInactiveAmbientBlur: string;
+  shadowWindowInactiveKeyBlur: string;
+  shadowWindowInactiveKeyX: string;
+  shadowWindowInactiveKeyY: string;
 };
 
 export type Shadow = {
-  shadowFlyout: string;
-  shadowToolbar: string;
-  shadowCardRest: string;
+  shadowCardDisabled: string;
   shadowCardHover: string;
   shadowCardPressed: string;
-  shadowCardDisabled: string;
+  shadowCardRest: string;
   shadowCtrlOnDrag: string;
+  shadowFlyout: string;
+  shadowFlyoutCss: string;
   shadowLayer: string;
+  shadowToolbar: string;
   shadowWindowActive: string;
   shadowWindowInactive: string;
 };
 
 export const shadowParts: ShadowParts = {
-  shadowFlyoutKeyX: s.shadowKeyX,
-  shadowFlyoutKeyY: s.shadowKeyY16,
-  shadowFlyoutKeyBlur: s.shadowKeyBlur16,
-  shadowFlyoutAmbientX: s.shadowAmbientX,
-  shadowFlyoutAmbientY: s.shadowAmbientY,
-  shadowFlyoutAmbientBlur: s.shadowAmbientBlurLow,
-  shadowToolbarKeyX: s.shadowKeyX,
-  shadowToolbarKeyY: s.shadowKeyY8,
-  shadowToolbarKeyBlur: s.shadowKeyBlur8,
-  shadowToolbarAmbientX: s.shadowAmbientX,
-  shadowToolbarAmbientY: s.shadowAmbientY,
-  shadowToolbarAmbientBlur: s.shadowAmbientBlurLow,
-  shadowCardRestKeyX: '0px',
-  shadowCardRestKeyY: '1px',
-  shadowCardRestKeyBlur: '2px',
-  shadowCardRestAmbientX: '0px',
-  shadowCardRestAmbientY: '0px',
-  shadowCardRestAmbientBlur: '2px',
-  shadowCardHoverKeyX: '0px',
-  shadowCardHoverKeyY: '3px',
-  shadowCardHoverKeyBlur: '10px',
-  shadowCardPressedKeyX: '0px',
-  shadowCardPressedKeyY: '1px',
-  shadowCardPressedKeyBlur: '8px',
+  shadowCardDisabledKeyBlur: '2px',
   shadowCardDisabledKeyX: '0px',
   shadowCardDisabledKeyY: '1px',
-  shadowCardDisabledKeyBlur: '2px',
-  shadowCtrlOnDragKeyX: '{shadowFlyoutKeyX}',
-  shadowCtrlOnDragKeyY: '{shadowFlyoutKeyY}',
-  shadowCtrlOnDragKeyBlur: '{shadowFlyoutKeyBlur}',
+  shadowCardHoverKeyBlur: '10px',
+  shadowCardHoverKeyX: '0px',
+  shadowCardHoverKeyY: '3px',
+  shadowCardPressedKeyBlur: '8px',
+  shadowCardPressedKeyX: '0px',
+  shadowCardPressedKeyY: '1px',
+  shadowCardRestAmbientBlur: '2px',
+  shadowCardRestAmbientX: '0px',
+  shadowCardRestAmbientY: '0px',
+  shadowCardRestKeyBlur: '2px',
+  shadowCardRestKeyX: '0px',
+  shadowCardRestKeyY: '1px',
+  shadowCtrlOnDragAmbientBlur: '{shadowFlyoutAmbientBlur}',
   shadowCtrlOnDragAmbientX: '{shadowFlyoutAmbientX}',
   shadowCtrlOnDragAmbientY: '{shadowFlyoutAmbientY}',
-  shadowCtrlOnDragAmbientBlur: '{shadowFlyoutAmbientBlur}',
-  shadowLayerKeyX: '0px',
-  shadowLayerKeyY: '1px',
-  shadowLayerKeyBlur: '2px',
+  shadowCtrlOnDragKeyBlur: '{shadowFlyoutKeyBlur}',
+  shadowCtrlOnDragKeyX: '{shadowFlyoutKeyX}',
+  shadowCtrlOnDragKeyY: '{shadowFlyoutKeyY}',
+  shadowFlyoutAmbientBlur: s.shadowAmbientBlurLow,
+  shadowFlyoutAmbientX: s.shadowAmbientX,
+  shadowFlyoutAmbientY: s.shadowAmbientY,
+  shadowFlyoutKeyBlur: s.shadowKeyBlur16,
+  shadowFlyoutKeyX: s.shadowKeyX,
+  shadowFlyoutKeyY: s.shadowKeyY16,
+  shadowLayerAmbientBlur: '2px',
   shadowLayerAmbientX: '0px',
   shadowLayerAmbientY: '0px',
-  shadowLayerAmbientBlur: '2px',
-  shadowWindowActiveKeyX: '0px',
-  shadowWindowActiveKeyY: '3px',
-  shadowWindowActiveKeyBlur: '12px',
+  shadowLayerKeyBlur: '2px',
+  shadowLayerKeyX: '0px',
+  shadowLayerKeyY: '1px',
+  shadowToolbarAmbientBlur: s.shadowAmbientBlurLow,
+  shadowToolbarAmbientX: s.shadowAmbientX,
+  shadowToolbarAmbientY: s.shadowAmbientY,
+  shadowToolbarKeyBlur: s.shadowKeyBlur8,
+  shadowToolbarKeyX: s.shadowKeyX,
+  shadowToolbarKeyY: s.shadowKeyY8,
+  shadowWindowActiveAmbientBlur: '28px',
   shadowWindowActiveAmbientX: '0px',
   shadowWindowActiveAmbientY: '20px',
-  shadowWindowActiveAmbientBlur: '28px',
-  shadowWindowInactiveKeyX: '0px',
-  shadowWindowInactiveKeyY: '3px',
-  shadowWindowInactiveKeyBlur: '12px',
+  shadowWindowActiveKeyBlur: '12px',
+  shadowWindowActiveKeyX: '0px',
+  shadowWindowActiveKeyY: '3px',
+  shadowWindowInactiveAmbientBlur: '12px',
   shadowWindowInactiveAmbientX: '0px',
   shadowWindowInactiveAmbientY: '3px',
-  shadowWindowInactiveAmbientBlur: '12px',
+  shadowWindowInactiveKeyBlur: '12px',
+  shadowWindowInactiveKeyX: '0px',
+  shadowWindowInactiveKeyY: '3px',
 };
 
 export const shadow: Shadow = {
-  shadowFlyout:
-    '{shadowFlyoutKeyX} {shadowFlyoutKeyY} {shadowFlyoutKeyBlur} 0px {shadowFlyoutKeyColor}, {shadowFlyoutAmbientX} {shadowFlyoutAmbientY} {shadowFlyoutAmbientBlur} 0px {shadowFlyoutAmbientColor}',
-  shadowToolbar:
-    '{shadowToolbarKeyX} {shadowToolbarKeyY} {shadowToolbarKeyBlur} 0px {shadowToolbarKeyColor}, {shadowToolbarAmbientX} {shadowToolbarAmbientY} {shadowToolbarAmbientBlur} 0px {shadowToolbarAmbientColor}',
-  shadowCardRest:
-    '{shadowCardRestKeyX} {shadowCardRestKeyY} {shadowCardRestKeyBlur} 0px {shadowCardRestKeyColor}, {shadowCardRestAmbientX} {shadowCardRestAmbientY} {shadowCardRestAmbientBlur} 0px {shadowCardRestAmbientColor}',
+  shadowCardDisabled:
+    '{shadowCardDisabledKeyX} {shadowCardDisabledKeyY} {shadowCardDisabledKeyBlur} 0px {shadowCardDisabledKeyColor}, {shadowCardRestAmbientX} {shadowCardRestAmbientY} {shadowCardRestAmbientBlur} 0px {shadowCardRestAmbientColor}',
   shadowCardHover:
     '{shadowCardHoverKeyX} {shadowCardHoverKeyY} {shadowCardHoverKeyBlur} 0px {shadowCardHoverKeyColor}, {shadowCardRestAmbientX} {shadowCardRestAmbientY} {shadowCardRestAmbientBlur} 0px {shadowCardRestAmbientColor}',
   shadowCardPressed:
     '{shadowCardPressedKeyX} {shadowCardPressedKeyY} {shadowCardPressedKeyBlur} 0px {shadowCardPressedKeyColor}, {shadowCardRestAmbientX} {shadowCardRestAmbientY} {shadowCardRestAmbientBlur} 0px {shadowCardRestAmbientColor}',
-  shadowCardDisabled:
-    '{shadowCardDisabledKeyX} {shadowCardDisabledKeyY} {shadowCardDisabledKeyBlur} 0px {shadowCardDisabledKeyColor}, {shadowCardRestAmbientX} {shadowCardRestAmbientY} {shadowCardRestAmbientBlur} 0px {shadowCardRestAmbientColor}',
+  shadowCardRest:
+    '{shadowCardRestKeyX} {shadowCardRestKeyY} {shadowCardRestKeyBlur} 0px {shadowCardRestKeyColor}, {shadowCardRestAmbientX} {shadowCardRestAmbientY} {shadowCardRestAmbientBlur} 0px {shadowCardRestAmbientColor}',
   shadowCtrlOnDrag:
     '{shadowCtrlOnDragKeyX} {shadowCtrlOnDragKeyY} {shadowCtrlOnDragKeyBlur} 0px {shadowCtrlOnDragKeyColor}, {shadowCtrlOnDragAmbientX} {shadowCtrlOnDragAmbientY} {shadowCtrlOnDragAmbientBlur} 0px {shadowCtrlOnDragAmbientColor}',
+  shadowFlyout:
+    '{shadowFlyoutKeyX} {shadowFlyoutKeyY} {shadowFlyoutKeyBlur} 0px {shadowFlyoutKeyColor}, {shadowFlyoutAmbientX} {shadowFlyoutAmbientY} {shadowFlyoutAmbientBlur} 0px {shadowFlyoutAmbientColor}',
+  shadowFlyoutCss: '{shadowFlyout}',
   shadowLayer:
     '{shadowLayerKeyX} {shadowLayerKeyY} {shadowLayerKeyBlur} 0px {shadowLayerKeyColor}, {shadowLayerAmbientX} {shadowLayerAmbientY} {shadowLayerAmbientBlur} 0px {shadowLayerAmbientColor}',
+  shadowToolbar:
+    '{shadowToolbarKeyX} {shadowToolbarKeyY} {shadowToolbarKeyBlur} 0px {shadowToolbarKeyColor}, {shadowToolbarAmbientX} {shadowToolbarAmbientY} {shadowToolbarAmbientBlur} 0px {shadowToolbarAmbientColor}',
   shadowWindowActive:
     '{shadowWindowActiveKeyX} {shadowWindowActiveKeyY} {shadowWindowActiveKeyBlur} 0px {shadowWindowActiveKeyColor}, {shadowWindowActiveAmbientX} {shadowWindowActiveAmbientY} {shadowWindowActiveAmbientBlur} 0px {shadowWindowActiveAmbientColor}',
   shadowWindowInactive:
