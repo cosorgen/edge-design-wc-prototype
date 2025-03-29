9ccec659526f464eaf1a0c30f9ba5bbf64c4f81a
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2025-03-29 13:33:42 -0700

    Fix button size issue

--- packages/button/src/styles.ts
+++ packages/button/src/styles.ts
@@ -3,4 +3,16 @@ import { ButtonStyles } from '@mai-ui/button';
 
 export const styles = css`
   ${ButtonStyles}
+
+  :host(:is([state--icon], :state(icon))) {
+    box-sizing: border-box !important;
+    padding:
+      var(--smtc-padding-ctrl-texttop),
+      var(--smtc-padding-ctrl-horizontal-icononly); /* padding needs to be on the host for border box to work */
+  }
+
+  :host(:is([state--icon], :state(icon))) .content {
+    box-sizing: border-box;
+    padding: 0;
+  }
 `;
